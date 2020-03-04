0f42ed9ea502c08719cfc8ac5f2b9a86dc852dd7
lsutula <lukasz.sutula@unity.pl>
2020-03-04 13:43:02 +0100

    init project

---
+++ src/main/java/pl/unity/lsutula/order/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package pl.unity.lsutula.order.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
