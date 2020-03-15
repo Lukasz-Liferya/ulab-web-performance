b016790ac8914eb7f6dde831e23b0792892397db
lsutula <lukasz.sutula@unity.pl>
2020-03-15 13:49:10 +0100

    add h2 dependencies

--- src/main/java/pl/unity/ulab1/shopping/infrastructure/hibernate/HibernateCartRepository.java
+++ src/main/java/pl/unity/ulab1/shopping/infrastructure/hibernate/HibernateCartRepository.java
@@ -1,5 +1,7 @@
 package pl.unity.ulab1.shopping.infrastructure.hibernate;
 
+import java.util.Optional;
+
 import pl.unity.ulab1.shopping.domain.Cart;
 import pl.unity.ulab1.shopping.domain.CartID;
 import pl.unity.ulab1.shopping.domain.CartRepository;
@@ -10,5 +12,10 @@ import pl.unity.ulab1.shopping.domain.CartRepository;
 public class HibernateCartRepository implements CartRepository {
 	public Cart getCart(CartID cartID) {
 		return null;
+	}
+
+	@Override
+	public Optional<Cart> findCart(CartID cartID) {
+		return Optional.empty();
 	}
 }
